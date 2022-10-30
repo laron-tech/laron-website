897cc907d5f58c282f24675035bd65c3268348bb
Ade M Ramdani <hi@iamade.me>
2022-10-31 05:50:45 +0700

    update

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -1,3 +1,3 @@
-<div class="bg-transparent w-screen h-40 py-24 px-2 backdrop-blur-sm">
+<div class="bg-transparent w-screen h-40 py-24 px-2 backdrop-blur-sm text-gray-100">
 	<h1 class="text-sm">Website last updated: October 31, 2022</h1>
 </div>
