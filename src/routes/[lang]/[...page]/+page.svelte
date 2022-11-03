0bb450d8173a75d73cc9a545f994f4ff8439d031
i-am-ade <hi@iamade.me>
2022-11-03 22:24:42 +0700

    use favicon as meta image

--- src/routes/[lang]/[...page]/+page.svelte
+++ src/routes/[lang]/[...page]/+page.svelte
@@ -22,14 +22,14 @@
 	<meta property="og:url" content={url} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content="/laron.svg" />
+	<meta property="og:image" content="/favicon.png" />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content={url} />
 	<meta property="twitter:title" content={title} />
 	<meta property="twitter:description" content={description} />
-	<meta property="twitter:image" content="/laron.svg" />
+	<meta property="twitter:image" content="/favicon.png" />
 </svelte:head>
 
 {#if template === Template.home}
