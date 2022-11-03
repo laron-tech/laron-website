8225f9b98b6409395bc504b84fef29d44292dad4
i-am-ade <hi@iamade.me>
2022-11-03 20:09:36 +0700

    vercel fix 1

--- src/routes/[lang]/+layout.server.ts
+++ src/routes/[lang]/+layout.server.ts
@@ -2,7 +2,7 @@ import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ fetch, params }) => {
-  let res = await fetch('/contents?list');
+  let res = await fetch('/contents?list=true');
   let langs: string[] = await res.json();
   if (!langs.includes(params.lang)) {
     throw redirect(302, `/en/${params.lang}/${params.page}`);
--- src/routes/contents/+server.ts
+++ src/routes/contents/+server.ts
@@ -6,13 +6,14 @@ export const GET: RequestHandler = ({ url }) => {
   let base = 'contents/'
   let path = url.searchParams.get('path');
   let list = url.searchParams.get('list');
-  if (!exist(base + path)) {
-    throw error(404, 'Not found');
-  }
 
   if (list) {
     let dir = readDir(base);
     return json(dir);
+  }
+
+  if (!exist(base + path)) {
+    throw error(404, 'Not found');
   }
 
   let content = loadFile(base + path);
